b2318e19d512f820f6d93f57fa8ed556c37c6dc4
man27382210 <man27382210@gmail.com>
2013-08-28 14:05:10 +0800

    fix bug

--- public/javascript/pump/view.js
+++ public/javascript/pump/view.js
@@ -2897,7 +2897,7 @@
                 text = view.$('#post-note #note-content').val(),
                 to = view.$('#post-note #note-to').val(),
                 cc = view.$('#post-note #note-cc').val(),
-                checkbox = view.$('#modal-note #FBcheckbox').attr("checked"),
+                checkbox = view.$('#modal-place #FBcheckbox').attr("checked"),
                 selectPlace = $('.place-select .active').attr('fbid');
             facebookconnect.getPlaceLink(selectPlace, function(respone){
                 var addText = '--@ <a href="'+respone.link + '">' + respone.name + '</a>';
